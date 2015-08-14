15b95277d1e0542be5f741e62172ec7c914af48e
Alexander "Ace" Olofsson <ace@haxalot.com>
2015-08-14 23:31:05 +0200

    Starting to move over MiniLD61 files for menus

---
+++ Source/MiniLD61/Menu/MenuPane.cpp
@@ -0,0 +1,175 @@
+#include "MenuPane.hpp"
+
+#include <Base/Fonts.hpp>
+
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/Window/Event.hpp>
+
+const uint8_t SHADED_ALPHA = 24;
+const float SHADED_OFFSET  = -32;
+const uint8_t OPENED_ALPHA = 255;
+const float OPENED_OFFSET  = 64;
+const uint8_t CLOSED_ALPHA = 0;
+const float CLOSED_OFFSET  = 256;
+
+MenuPane::MenuPane() :
+	mOffset(Tween::BackOut), mAlpha(Tween::LinearIn), mState(Closed), mLastJoy(0), mSelection(-1), mEntryCount(-1)
+{
+}
+
+MenuPane::~MenuPane()
+{
+}
+
+void MenuPane::draw(sf::RenderTarget& target)
+{
+	sf::Text title(getName(), sf::getDefaultFont(), 30U);
+
+	title.setOrigin(0, 15);
+	title.setPosition(getOffset(), 64);
+	title.setColor(sf::Color(255,255,255,getAlpha()));
+
+	target.draw(title);
+}
+void MenuPane::handleEvent(sf::Event& ev)
+{
+	if (mEntryCount <= 0 || (mState > MenuPane::Open && mState != MenuPane::Unshading))
+		return;
+
+	if (ev.type == sf::Event::KeyPressed)
+	{
+		if (ev.key.code == sf::Keyboard::Up || ev.key.code == sf::Keyboard::W)
+		{
+			if (--mSelection < 0)
+				mSelection = mEntryCount - 1;
+		}
+		else if (ev.key.code == sf::Keyboard::Down || ev.key.code == sf::Keyboard::S)
+		{
+			if (++mSelection >= mEntryCount)
+				mSelection = 0;
+		}
+	}
+	else if (ev.type == sf::Event::JoystickMoved)
+	{
+		if (ev.joystickMove.axis == sf::Joystick::Axis::Y || ev.joystickMove.axis == sf::Joystick::Axis::PovY)
+		{
+			float len = ev.joystickMove.position;
+
+			if (len < -50 && mLastJoy > -25)
+			{
+				if (--mSelection < 0)
+					mSelection = mEntryCount - 1;
+				mLastJoy = len;
+			}
+			else if (len > 50 && mLastJoy < 25)
+			{
+				if (++mSelection >= mEntryCount)
+					mSelection = 0;
+				mLastJoy = len;
+			}
+			else if (len > -25 || len < 25)
+				mLastJoy = len;
+		}
+	}
+}
+void MenuPane::update(float dt)
+{
+	mOffset.update(dt);
+	mAlpha.update(dt);
+
+	if (mOffset.atEnd())
+	{
+		if (mState == Shading)
+			mState = Shaded;
+		else if (mState == Unshading || mState == Opening)
+			mState = Open;
+		else if (mState == Closing)
+			mState = Closed;
+	}
+}
+
+void MenuPane::setState(State s)
+{
+	const float ALPHA_DELAY = 0.5;
+	const float OFFSET_DELAY = 0.75;
+
+	mState = s;
+
+	switch(mState)
+	{
+	case Shading:
+		mOffset.setFunc(Tween::CubicOut);
+
+		mAlpha.start(ALPHA_DELAY, OPENED_ALPHA, SHADED_ALPHA);
+		mOffset.start(OFFSET_DELAY, OPENED_OFFSET, SHADED_OFFSET);
+		break;
+
+	case Shaded:
+		mAlpha.start(0, SHADED_ALPHA, SHADED_ALPHA);
+		mOffset.start(0, SHADED_OFFSET, SHADED_OFFSET);
+		break;
+
+	case Unshading:
+		mOffset.setFunc(Tween::QuadraticIn);
+
+		mAlpha.start(ALPHA_DELAY, SHADED_ALPHA, OPENED_ALPHA);
+		mOffset.start(OFFSET_DELAY, SHADED_OFFSET, OPENED_OFFSET);
+		break;
+
+	case Closing:
+		mOffset.setFunc(Tween::QuadraticIn);
+
+		mAlpha.start(ALPHA_DELAY, OPENED_ALPHA, CLOSED_ALPHA);
+		mOffset.start(OFFSET_DELAY, OPENED_OFFSET, CLOSED_OFFSET);
+		break;
+
+	case Closed:
+		mAlpha.start(0, CLOSED_ALPHA, CLOSED_ALPHA);
+		mOffset.start(0, CLOSED_OFFSET, CLOSED_OFFSET);
+		break;
+
+	case Opening:
+		mOffset.setFunc(Tween::BackOut);
+
+		mAlpha.start(ALPHA_DELAY, CLOSED_ALPHA, OPENED_ALPHA);
+		mOffset.start(OFFSET_DELAY, -100, OPENED_OFFSET);
+		break;
+
+	case Open:
+		mAlpha.start(0, OPENED_ALPHA, OPENED_ALPHA);
+		mOffset.start(0, OPENED_OFFSET, OPENED_OFFSET);
+		break;
+	}
+}
+MenuPane::State MenuPane::getState() const
+{
+	return mState;
+}
+
+float MenuPane::getOffset() const
+{
+	return *mOffset;
+}
+float MenuPane::getAlpha() const
+{
+	return *mAlpha;
+}
+
+char MenuPane::getSelection() const
+{
+	return mSelection;
+}
+void MenuPane::setSelection(char entry)
+{
+	mSelection = entry;
+}
+void MenuPane::setEntryCount(char count)
+{
+	mEntryCount = count;
+}
+MenuState& MenuPane::getMenuState() const
+{
+	return *mMenu;
+}
+
---
+++ Source/MiniLD61/Menu/MenuPane.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <Base/Tweening.hpp>
+
+#include <string>
+
+namespace sf
+{
+	struct Event;
+	class RenderTarget;
+}
+
+class MenuState;
+class MenuPane
+{
+public:
+	enum State
+	{
+		Opening,   // Coming in from the left
+		Open,      // Pane is open in the foreground
+		Shading,   // Sliding off to the left
+		Shaded,    // In the background on the left
+		Unshading, // Sliding back into focus
+		Closing,   // Disappearing off to the right
+		Closed     // Gone.
+	};
+
+	MenuPane();
+	virtual ~MenuPane();
+
+	virtual void draw(sf::RenderTarget&);
+	virtual void handleEvent(sf::Event&);
+	virtual void update(float dt);
+
+	virtual std::string getName() const = 0;
+
+	void setState(State s);
+	State getState() const;
+
+protected:
+	float getOffset() const;
+	float getAlpha() const;
+
+	char getSelection() const;
+	void setSelection(char entry);
+	void setEntryCount(char count);
+
+	MenuState& getMenuState() const;
+
+private:
+	MenuState* mMenu;
+	Easer mOffset, mAlpha;
+	State mState;
+
+	float mLastJoy;
+	char mSelection, mEntryCount;
+
+	friend class MenuState;
+};
+
