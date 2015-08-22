1f373ca2414a94aedc9407e109b80138dffa7d13
Alexander "Ace" Olofsson <ace@haxalot.com>
2015-08-22 19:49:07 +0200

    Add normalization

--- Source/Base/VectorMath.hpp
+++ Source/Base/VectorMath.hpp
@@ -8,4 +8,5 @@ namespace VMath
 	inline float Dot(const sf::Vector2f& a, const sf::Vector2f& b) { return a.x * a.x + a.y * a.y; }
 	inline float Distance(const sf::Vector2f& a, const sf::Vector2f& b) { sf::Vector2f diff{ a.x - b.x, a.y - b.y }; return Dot(diff, diff); }
 	inline float DistanceSqrt(const sf::Vector2f& a, const sf::Vector2f& b) { sf::Vector2f diff{ a.x - b.x, a.y - b.y }; return std::sqrt(Dot(diff, diff)); }
-}
+	inline sf::Vector2f GetNormalized(const sf::Vector2f& a) { float len = std::sqrt(Dot(a, a)); if (len == 0) return a; return a / len; }
+}	
