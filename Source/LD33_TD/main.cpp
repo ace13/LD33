36788aa0ad5b2b88a6372e110a22838d52843e70
Alexander "Ace" Olofsson <ace@haxalot.com>
2015-08-23 08:53:12 +0200

    Add some more music pieces

--- Source/LD33_TD/main.cpp
+++ Source/LD33_TD/main.cpp
@@ -16,12 +16,27 @@ int main(int argc, char** argv)
 	auto& mus = Engine::get<MusicManager>();
 	{
 		sf::SoundBuffer buf;
-		buf.loadFromFile("Resources/bass.wav");
-
+		buf.loadFromFile("Resources/bass.ogg");
 		mus.addTrack(0, std::move(buf));
+
+		buf.loadFromFile("Resources/bass2.ogg");
+		mus.addTrack(1, std::move(buf));
+
+		buf.loadFromFile("Resources/kicker.ogg");
+		mus.addTrack(2, std::move(buf));
+
+		buf.loadFromFile("Resources/loop.ogg");
+		mus.addTrack(3, std::move(buf));
+
+		buf.loadFromFile("Resources/test.ogg");
+		mus.addTrack(4, std::move(buf));
 	}
 
-	mus[0].setVolume(25.f, 4);
+	//mus[0].setVolume(.5f, 4);
+	//mus[1].setVolume(.5f, 4);
+	//mus[2].setVolume(.5f, 4);
+	mus[3].setVolume(10.f, 5);
+	//mus[4].setVolume(15.f, 10);
 
 	RegisterComponents(sys);
 
@@ -29,6 +44,7 @@ int main(int argc, char** argv)
 	eng.setWindow(win);
 
 	win.create(sf::VideoMode(800, 600), "Ludum Dare #33");
+
 #ifndef NDEBUG
 	win.setFramerateLimit(250);
 #endif
